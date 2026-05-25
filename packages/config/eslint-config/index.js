8234e96eeea4d8550f4dcdcdcabd3545fe6e9426
onegayunicorn <40648133+onegayunicorn@users.noreply.github.com>
2026-05-25 12:10:28 +1000

    Add packages/config/eslint-config/index.js

---
+++ packages/config/eslint-config/index.js
@@ -0,0 +1,12 @@
+{
+  "name": "@quantum/config-eslint",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "@typescript-eslint/eslint-plugin": "^6.15.0",
+    "@typescript-eslint/parser": "^6.15.0",
+    "eslint": "^8.56.0",
+    "eslint-plugin-react": "^7.33.2",
+    "eslint-plugin-react-hooks": "^4.6.0"
+  }
+}
